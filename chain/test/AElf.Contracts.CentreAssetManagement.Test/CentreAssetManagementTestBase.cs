84ecb15d0f1e9a78e9714bd3d006840b01872019
yiqi <yiqi.zhao@aelf.io>
2020-04-08 16:49:07 +0800

    Add initial token contract logic in test project.

--- chain/test/AElf.Contracts.CentreAssetManagement.Test/CentreAssetManagementTestBase.cs
+++ chain/test/AElf.Contracts.CentreAssetManagement.Test/CentreAssetManagementTestBase.cs
@@ -2,9 +2,11 @@ using System.IO;
 using System.Linq;
 using Acs0;
 using AElf.Blockchains.BasicBaseChain.ContractNames;
+using AElf.Contracts.MultiToken;
 using AElf.Contracts.TestKit;
 using AElf.Cryptography.ECDSA;
 using AElf.Kernel;
+using AElf.Kernel.Token;
 using AElf.Types;
 using Google.Protobuf;
 using Volo.Abp.Threading;
@@ -14,9 +16,11 @@ namespace AElf.Contracts.CentreAssetManagement
     public class CentreAssetManagementTestBase : ContractTestBase<CentreAssetManagementTestModule>
     {
         internal CentreAssetManagementContainer.CentreAssetManagementStub CentreAssetManagementStub { get; set; }
+        internal TokenContractContainer.TokenContractStub TokenContractStub { get; set; }
         private ACS0Container.ACS0Stub ZeroContractStub { get; set; }
 
         private Address CentreAssetManagementAddress { get; set; }
+        private Address TokenContractAddress { get; set; }
 
         protected CentreAssetManagementTestBase()
         {
@@ -38,6 +42,17 @@ namespace AElf.Contracts.CentreAssetManagement
                             new SystemContractDeploymentInput.Types.SystemTransactionMethodCallList()
                     })).Output;
             CentreAssetManagementStub = GetCentreAssetManagementStub(SampleECKeyPairs.KeyPairs.First());
+            
+            TokenContractAddress = AsyncHelper.RunSync(() =>
+                ZeroContractStub.DeploySystemSmartContract.SendAsync(
+                    new SystemContractDeploymentInput
+                    {
+                        Category = KernelConstants.DefaultRunnerCategory,
+                        Code = ByteString.CopyFrom(File.ReadAllBytes(typeof(TokenContract).Assembly.Location)),
+                        Name = TokenSmartContractAddressNameProvider.Name,
+                        TransactionMethodCallList = GetTokenContractInitialMethodCallList()
+                    })).Output;
+            TokenContractStub = GetTokenContractStub(SampleECKeyPairs.KeyPairs.First());
         }
 
         private ACS0Container.ACS0Stub GetZeroContractStub(ECKeyPair keyPair)
@@ -48,6 +63,39 @@ namespace AElf.Contracts.CentreAssetManagement
         private CentreAssetManagementContainer.CentreAssetManagementStub GetCentreAssetManagementStub(ECKeyPair keyPair)
         {
             return GetTester<CentreAssetManagementContainer.CentreAssetManagementStub>(CentreAssetManagementAddress, keyPair);
+        }
+        
+        private TokenContractContainer.TokenContractStub GetTokenContractStub(ECKeyPair keyPair)
+        {
+            return GetTester<TokenContractContainer.TokenContractStub>(CentreAssetManagementAddress, keyPair);
+        }
+
+        private SystemContractDeploymentInput.Types.SystemTransactionMethodCallList
+            GetTokenContractInitialMethodCallList()
+        {
+            return new SystemContractDeploymentInput.Types.SystemTransactionMethodCallList
+            {
+                Value =
+                {
+                    new SystemContractDeploymentInput.Types.SystemTransactionMethodCall
+                    {
+                        MethodName = nameof(TokenContractStub.Create),
+                        Params = new CreateInput
+                        {
+                            // Issue
+                            Issuer = ContractZeroAddress
+                        }.ToByteString()
+                    },
+                    new SystemContractDeploymentInput.Types.SystemTransactionMethodCall
+                    {
+                        MethodName = nameof(TokenContractStub.Issue),
+                        Params = new IssueInput
+                        {
+                            
+                        }.ToByteString()
+                    }
+                }
+            };
         }
     }
 }
